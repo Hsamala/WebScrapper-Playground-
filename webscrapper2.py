cd4757a22eaad60f6ac14f61be8f3307f4629abb
Hsamala <harrys7354@gmail.com>
2024-07-02 22:03:00 -0600

    Added CSV data entry

--- webscrapper2.py
+++ webscrapper2.py
@@ -1,6 +1,8 @@
-import requests, sys
-import pdb
+import requests, sys, os
+import pdb, csv
 from bs4 import BeautifulSoup
+
+newJson = "new_scrapped_data.csv"
 
 baseURL = "https://remote.co/remote-jobs/"
 
@@ -9,13 +11,13 @@ jobBuckets = ["accounting", "customer service", "online-data-entry", "design",
               "marketing", "project-manager", "qa", "sales", "virtual-assistant",
               "writing", "other"]
 
-def connectSite(job: str):
+def connectSite(job: str) -> BeautifulSoup:
     currentURL = baseURL + job + "/"
     currentPage = requests.get(currentURL)
     currentHTMLSoup = BeautifulSoup(currentPage.content, "html.parser")
     return currentHTMLSoup
 
-def parseIntialLandingSoup(currentSoup: BeautifulSoup, tagSearch: str):
+def parseIntialLandingSoup(currentSoup: BeautifulSoup, tagSearch: str) -> BeautifulSoup:
     card_BodyClass = currentSoup.find("div", class_="card bg-light mb-3 rounded-0")
     job_HTML = card_BodyClass.find_all("a", class_="card m-0 border-left-0 border-right-0 border-top-0 border-bottom")
 
@@ -27,8 +29,15 @@ def parseIntialLandingSoup(currentSoup: BeautifulSoup, tagSearch: str):
             return actualJobPageHTMLSoup
     return None
 
-def parseJobPostingSoup(JobSiteSoup: BeautifulSoup):
-    jobInfo = []
+def parseJobPostingSoup(JobSiteSoup: BeautifulSoup) -> list:
+    jobInfo = {
+        "Job Name" : "",
+        "Job Location" : "", 
+        "Job Posted Date": "",
+        "Job Salary": "", 
+        "Company Link": "",
+        "Apply Link": ""
+    }
     #Traversing through the general site:
     job_Name = JobSiteSoup.find("h1", class_="font-weight-bold")
     company_Link = JobSiteSoup.find("div", class_="links_sm").a["href"]
@@ -40,34 +49,73 @@ def parseJobPostingSoup(JobSiteSoup: BeautifulSoup):
     job_Posted_Date = job_info_container.find("time")["datetime"]
     job_Salary = job_info_container.find("div", class_="salary_sm row")
 
-    #Add everything to the job List
-    jobInfo.append(job_Name)
-    jobInfo.append(job_Location)
-    jobInfo.append(job_Posted_Date) 
-    jobInfo.append(job_Salary)
-    jobInfo.append(company_Link)
-    jobInfo.append(apply_Link)
+    #Add everything to the jobInfo dictionary
+    jobInfo["Job Name"] = job_Name
+    jobInfo["Job Location"] = job_Location
+    jobInfo["Job Posted Date"] = job_Posted_Date 
+    jobInfo["Job Salary"] = job_Salary
+    jobInfo["Company Link"] = company_Link
+    jobInfo["Apply Link"] = apply_Link
 
-    for item in jobInfo:
-        if(item == None):
-            continue
-        elif(type(item) == str):
-            print(item)
+    return jobInfo
+
+def writeToCSV(jobInfo: dict, csv_File: str):
+
+    for key in jobInfo:
+        if( type(jobInfo[key]) == str):
+            jobInfo[key] = jobInfo[key]
+        elif ( jobInfo[key] == None):
+            jobInfo[key] = "None"
         else:
-            print(item.text.strip())
+            jobInfo[key] = str(jobInfo[key].text.strip())
+    
+    write_file_size = os.path.getsize(newJson)
 
-    print('---------------------------------------------------------------------')
+    if(write_file_size == 0): 
+        with open(csv_File, "w", newline="") as csv_file:
+            csv_writer = csv.writer(csv_file)
+        
+            for key, value in jobInfo.items():
+                csv_writer.writerow([key, value])
+    else:
+       with open(csv_File, "a", newline="") as csv_file:
+            csv_writer = csv.writer(csv_file)
+        
+            for key, value in jobInfo.items():
+                csv_writer.writerow([key, value]) 
+
+
+def readAndCompare(jobInfo: dict, csv_File: str):
+
+    with open(csv_File, 'r') as currentFile:
+        csv_Reader = csv.DictReader(currentFile)
+
+        for row in csv_Reader:
+           print(row)       
+
 
 def main():
     for job in jobBuckets:
-        #pdb.set_trace()
+        #
         htmlSoup = connectSite(job)
+
         jobPageSoup = parseIntialLandingSoup(htmlSoup, str(sys.argv[1]))
+
+        jobInfoDict = None
+
         if(jobPageSoup == None):
             continue
         else: 
-            parseJobPostingSoup(jobPageSoup)
-    
+            jobInfoDict = parseJobPostingSoup(jobPageSoup)
+
+        if(jobInfoDict == None):
+            continue
+        else:
+            writeToCSV(jobInfoDict, newJson)
+
+        readAndCompare(jobInfoDict, newJson)
+
+
 
 if __name__ == "__main__":
     main()
